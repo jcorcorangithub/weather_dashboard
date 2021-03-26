5977d7740dba6dd78b4bd88f326d206d19773ad0
James Corcoran <jpcsoccer1234@gmail.com>
2021-03-26 15:57:54 -0400

    specified a click function for what will be the search button, correctly coded the fetch function for requesting weather API info for a city

---
+++ script.js
@@ -0,0 +1,27 @@
+let cityInput = $("city-input");
+
+$("#search-button").on("click", renderSearch);
+
+
+function renderSearch(event){
+    event.preventDefault();
+
+    let city = cityInput.val();
+    let url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=1db5fbc9e14543777ea71f3e4b794636";
+
+    fetch(url)
+        .then(function (response){
+            if(response.status != 200){
+                //change this so that it is not a window alert
+                alert("Invalid city");
+            }
+            return response.json();
+
+        })
+        .then(function(data){
+            //console.log(data);
+            $("p").text("city weather " + city.weather);
+        })
+}
+
+
